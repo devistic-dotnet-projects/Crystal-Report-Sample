ea4ac28b0e8859c185e416fca5b9b570bade3e3e
Muhammad Asim <asimkamal251@gmail.com>
2023-09-05 23:57:28 +0500

    code aded

---
+++ CReport/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using CReport.Models;
+using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalReportMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //DbContext  
+        private CustomerDBEntities context = new CustomerDBEntities();
+        // GET: Customer  
+        public ActionResult Index()
+        {
+            var customerList = context.Customers.ToList();
+            return View(customerList);
+        }
+
+        public ActionResult ExportCustomers()
+        {
+            List<Customer> allCustomer = new List<Customer>();
+            allCustomer = context.Customers.ToList();
+
+            ReportDocument rd = new ReportDocument();
+            string path= Path.Combine(Server.MapPath("~/CrystalReport"), "ReportCustomer.rpt");
+
+            rd.Load(path);
+
+            rd.SetDataSource(allCustomer.ToList());
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            try
+            {
+                Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "CustomerList.pdf");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
---
+++ CReport/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CReport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CReport")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7597f2b9-c3eb-4f0c-a5e8-a9fa8a0e0dd8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
